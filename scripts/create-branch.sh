0282aa0b9836f801f07d297565bb4100237bd773
gounthar <gounthar@gmail.com>
2022-12-10 18:42:34 +0100

    feat(scripts) Only creates the branch now

--- scripts/create-branch.sh
+++ scripts/create-branch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
+# create a new branch (or check if exists) thanks to gh cli
+
 echo "$GITHUB_CREDENTIALS_PSW" | gh auth login --with-token
-cp /tmp/sorted_jenkins_plugins.txt ./plugins.txt
-git add .
 # Get current branch name
 branch_name=$GIT_BRANCH
 echo "Working on branch $branch_name for repo $GIT_URL"
@@ -10,8 +10,3 @@ new_branch_name="binaries-for-$branch_name"
 git config --global user.email "116569+gounthar@users.noreply.github.com"
 git config --global user.name "$GITHUB_CREDENTIALS_USR"
 git switch -c "${new_branch_name}" -m
-git commit -m "chore(binaries) New binaries for $branch_name"
-git push --set-upstream https://"$GITHUB_CREDENTIALS_USR":"$GITHUB_CREDENTIALS_PSW"@github.com/$GITHUB_REPOSITORY.git "${new_branch_name}"
-# Now use gh to create a pull request from new_branch_name to branch_name
-#gh pr create -B "$branch_name" -t "Update plugins.txt" -b "Update plugins.txt" --head "${new_branch_name}" --base "$branch_name"
-#git switch "${branch_name}"
