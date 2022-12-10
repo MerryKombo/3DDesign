cf7264a6f636ff2763269a3c22dcafe5579b613b
gounthar <gounthar@gmail.com>
2022-12-10 21:52:21 +0100

    fix(action) Added environment variables

--- scripts/create-branch.sh
+++ scripts/create-branch.sh
@@ -7,6 +7,8 @@ echo "$GH_CREDENTIALS_PSW" | gh auth login --with-token
 # Get current branch name
 branch_name=$BRANCH_NAME
 echo "Working on branch $branch_name for repo $GIT_URL"
+echo "$(pwd) is the current directory"
+echo "Checking if branch $branch_name exists"
 new_branch_name="binaries-for-$branch_name"
 git config --global user.email "116569+gounthar@users.noreply.github.com"
 git config --global user.name "$GH_CREDENTIALS_PSW"
