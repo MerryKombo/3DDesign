2acc82e66186dd6c20bf81403fbf78364280d78c
gounthar <gounthar@gmail.com>
2022-12-10 18:43:23 +0100

    feat(scripts) Now calls the other scripts

--- scripts/generate-binaries.sh
+++ scripts/generate-binaries.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
 find . -type f -name "*scad" -print0 | xargs -I{} --null xvfb-run -a openscad --imgsize=4096,2160 {} -o {}.png
 find . -type f -name "*scad" -print0 | xargs -I{} --null openscad {} -o {}.stl
+
+create-branch.sh
+keep-only-binaries.sh
+push-to-repo.sh
