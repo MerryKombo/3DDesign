cbca716f9d89e4e3282b9a3aedac90795e0d02e7
Bruno Verachten <gounthar@gmail.com>
2025-05-29 20:38:02 +0200

    fix(script): Ensure `keep-only-binaries.sh` creates binaries directory before syncing files

--- dockerfiles/scripts/keep-only-binaries.sh
+++ dockerfiles/scripts/keep-only-binaries.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 echo "Starting the keep-only-binaries.sh script"
 
+# Create binaries directory if it doesn't exist
+mkdir -p binaries
+
 # and move only the binary files while keeping the same directory structure
 rsync -rv --include '*/' --include '*.png' --include '*.stl' --exclude '*' --prune-empty-dirs . binaries/
 
