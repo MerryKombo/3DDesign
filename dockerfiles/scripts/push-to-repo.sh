fdfe81e96577a95c4a419c3494b974713c13f1b1
Bruno Verachten <gounthar@gmail.com>
2025-05-29 20:38:20 +0200

    fix(script): Update authentication method and enhance branch creation for `push-to-repo.sh`

--- dockerfiles/scripts/push-to-repo.sh
+++ dockerfiles/scripts/push-to-repo.sh
@@ -5,12 +5,12 @@ echo "Starting the push-to-repo.sh script"
 git add .
 
 # Get current branch name
-branch_name=$GIT_BRANCH
+branch_name=$BRANCH_NAME
+new_branch_name="binaries-for-$branch_name"
 
 git commit -m "chore(binaries) New binaries for $branch_name"
-git push --set-upstream https://"$GITHUB_CREDENTIALS_USR":"$GITHUB_CREDENTIALS_PSW"@github.com/$GITHUB_REPOSITORY.git "${new_branch_name}"
+git push --set-upstream https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git "${new_branch_name}"
 # Now use gh to create a pull request from new_branch_name to branch_name
-#gh pr create -B "$branch_name" -t "Update plugins.txt" -b "Update plugins.txt" --head "${new_branch_name}" --base "$branch_name"
-#git switch "${branch_name}"
+gh pr create -B "$branch_name" -t "Update binaries for $branch_name" -b "Update generated binaries for $branch_name" --head "${new_branch_name}" --base "$branch_name"
 
-echo "Starting the push-to-repo.sh script"
+echo "Ending the push-to-repo.sh script"
