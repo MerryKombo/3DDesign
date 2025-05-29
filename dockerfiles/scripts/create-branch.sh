bbca2241ff96402ed18f03fcde97a9752831b0c1
Bruno Verachten <gounthar@gmail.com>
2025-05-29 20:37:30 +0200

    fix(script): Update authentication method and improve branch creation logic in `create-branch.sh`

--- dockerfiles/scripts/create-branch.sh
+++ dockerfiles/scripts/create-branch.sh
@@ -3,16 +3,23 @@
 # create a new branch (or check if exists) thanks to gh cli
 echo "Starting the create-branch.sh script"
 
-echo "$GH_CREDENTIALS_PSW" | gh auth login --with-token
+# Authenticate with GitHub using GITHUB_TOKEN
+echo "$GITHUB_TOKEN" | gh auth login --with-token
+
 # Get current branch name
 branch_name=$BRANCH_NAME
 echo "Working on branch $branch_name for repo $GIT_URL"
 echo "$(pwd) is the current directory"
 echo "Checking if branch $branch_name exists"
-echo "$HOME is the home directory"
+
+# Create a new branch name for the binaries
 new_branch_name="binaries-for-$branch_name"
-git config --global user.email "116569+gounthar@users.noreply.github.com"
-git config --global user.name "$GH_CREDENTIALS_PSW"
-git switch -c "${new_branch_name}" -m
+
+# Configure Git with a temporary config file to avoid permission issues
+git config --local user.email "github-actions@github.com"
+git config --local user.name "GitHub Actions"
+
+# Create a new branch for the binaries
+git checkout -b "${new_branch_name}"
 
 echo "Ending the create-branch.sh script"
